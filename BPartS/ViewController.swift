e392590a83db62c8e35c3a2e6a65bb6d10281722
Theodore Abshire <TheodoreAbshire@Gmail.com>
2016-08-02 10:30:32 -0700

    Initial Commit

---
+++ BPartS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BPartS
+//
+//  Created by Theodore Abshire on 8/2/16.
+//  Copyright © 2016 Theodore Abshire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
