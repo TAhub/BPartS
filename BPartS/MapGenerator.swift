91c87fb4c2f0575c8e67df4143e6923a569bedf4
Theodore Abshire <TheodoreAbshire@Gmail.com>
2016-08-08 20:22:30 -0700

    Added the skeleton of the map room placement system.

--- BPartS/MapGenerator.swift
+++ BPartS/MapGenerator.swift
@@ -10,6 +10,26 @@ import UIKit
 
 let relativeLevelAdjustFactor:CGFloat = 0.05
 
+//TODO: move the room structure to a map class
+class Room
+{
+	let x:Int
+	let y:Int
+	weak var north:Room?
+	weak var east:Room?
+	weak var west:Room?
+	weak var south:Room?
+	
+	//TODO: encounter data
+	//TODO: room type data
+	
+	init(x:Int, y:Int)
+	{
+		self.x = x
+		self.y = y
+	}
+}
+
 class MapGenerator
 {
 	static func generateEncounter(encounterData:[Int], areaLevel:Int) -> [Creature]
@@ -27,5 +47,92 @@ class MapGenerator
 		
 		
 		return creatures
+	}
+	
+	static func generateRooms()
+	{
+		var rooms = [Room]()
+		var failed = false
+		
+		//constants
+		let branchLength = 5
+		let mapLength = 20
+		
+		func recursiveRoomGenerate(x x:Int, y:Int, branchType:String?, obligations:[String], lengthLeft:Int, roomFrom:Room?)
+		{
+			if failed
+			{
+				return
+			}
+			
+			let room = Room(x: x, y: y)
+			rooms.append(room)
+			
+			if let roomFrom = roomFrom
+			{
+				//TODO: set the north, south, etc properties of both room and roomFrom to link them to each other
+				
+				if lengthLeft == 0
+				{
+					if let branchType = branchType
+					{
+						//TODO: make this a special room type, based on the branch type
+						//IE a key branch will have a key here, etc
+					}
+					else
+					{
+						//TODO: make this the exit room
+					}
+					return
+				}
+			}
+			else
+			{
+				//TODO: make room into a start room type
+			}
+			
+			
+			var possibleMoves = [(Int, Int)]()
+			//TODO: find every possible direction you can expand (AKA, direction without a room already there)
+			
+			if possibleMoves.count == 0
+			{
+				failed = true
+				return
+			}
+			
+			//TODO: randomly re-order the possible moves
+			
+			let mustFulfillObligation = obligations.count >= lengthLeft - 1 //if you get too close to the end, do nothing but fulfill obligations
+			var obligations = obligations
+			if possibleMoves.count >= 2 && branchType == nil && !mustFulfillObligation //TODO: only have a chance of doing this; maybe a max number of branches too?
+			{
+				let branchMove = possibleMoves[1]
+				let branchType = "key" //TODO: pick a branch type
+				//TODO: branch types should have associated information; like the color of the key for a key branch, etc
+				obligations.append(branchType)
+				recursiveRoomGenerate(x: branchMove.0, y: branchMove.1, branchType: branchType, obligations: [String](), lengthLeft: branchLength, roomFrom: room)
+			}
+			else if obligations.count > 0
+			{
+				//TODO: there should be a chance to remove one obligation and "fulfill" it
+				//unless mustFulfillObligation is true, then it should be a 100% chance
+				//IE a "key" obligation turns this into a gate room that requires a key to move to the next room
+			}
+			
+			let nextMove = possibleMoves[0]
+			recursiveRoomGenerate(x: nextMove.0, y: nextMove.1, branchType: nil, obligations: obligations, lengthLeft: lengthLeft - 1, roomFrom: room)
+		}
+		
+		recursiveRoomGenerate(x: 0, y: 0, branchType: nil, obligations: [String](), lengthLeft: mapLength, roomFrom: nil)
+		
+		if failed
+		{
+			//TODO: try again
+		}
+		else
+		{
+			//TODO: return the rooms I guess
+		}
 	}
 }
